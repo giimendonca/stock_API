4bc8c08ad545d3ddcca70dcb58706eace3a7cbd4
giovana_mendonca <giovanamendoncaraimundo@gmail.com>
2026-08-17 17:27:19 -0300

    feat: function verificar Token

--- includes/functions.php
+++ includes/functions.php
@@ -7,3 +7,31 @@ function jsonReturn($msg, $httpCode){
     exit();
 }
 
+function verificarToken($conn){
+    $headers =  getallheaders();
+
+    if(!isset($headers['Authorization'])){
+        jsonReturn(["erro" => "Token inválido."], 401);
+    }
+
+    $token = str_replace("Bearer ", "", $headers['Authorization']);
+
+    if(empty($token)){
+        jsonReturn(["erro" => "Token inválido."], 401);
+    }
+
+    $sql = "SELECT id, nome, email FROM usuarios WHERE token = ?";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $token);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $usuario = $result->fetch_assoc();
+
+    if(!$usuario){
+        jsonReturn(["erro" => "Token inválido."], 401);
+    }
+
+    return $usuario;
+}
